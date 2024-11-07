d7e588b8593c21a5f88f3abeb3b0ef6a547c59f7
Yuxin-Ji <ji.yuxin@hotmail.com>
2024-11-07 17:08:26 -0600

    project2

--- project2/snowflake_to_snowflake.py
+++ project2/snowflake_to_snowflake.py
@@ -17,30 +17,60 @@ DAG_ID = "project2_snowflake_to_snowflake"
 SQL_FILE = "sql_op_str.sql"
 # SQL commands
 # @TODO: our own SQL statement
-UPDATE_TABLE_SQL_STRING = (
-    f"UPDATE {SNOWFLAKE_TARGET_TABLE} SET name = 'new_name' WHERE id = 1;"
+CREATE_TABLE_SQL_STRING = (
+    f'''CREATE OR REPLACE TABLE AIRFLOW1007.BF_DEV.FACT_STOCK_HISTORY_TEAM6 (
+            SYMBOL VARCHAR(16) NOT NULL,
+            TRADEDATE DATE NOT NULL,
+            OPEN FLOAT,
+            HIGH FLOAT,
+            LOW FLOAT,
+            CLOSE FLOAT,
+            VOLUME NUMBER(38,0),
+            ADJCLOSE FLOAT,
+            primary key (SYMBOL, TRADEDATE),
+            foreign key (SYMBOL) references AIRFLOW1007.BF_DEV.DIM_SYMBOLS_TEAM6(SYMBOL)
+        );
+        create or replace TABLE AIRFLOW1007.BF_DEV.DIM_COMPANY_PROFILE_TEAM6 (
+            ID NUMBER(38,0) NOT NULL,
+            SYMBOL VARCHAR(16),
+            PRICE FLOAT,
+            BETA FLOAT,
+            VOLAVG FLOAT,
+            INDUSTRY VARCHAR(16777216),
+            SECTOR VARCHAR(16777216),
+            WEBSITE VARCHAR(16777216),
+            NAME VARCHAR(16777216),
+            primary key (ID)
+        );
+        create or replace TABLE AIRFLOW1007.BF_DEV.DIM_SYMBOLS_TEAM6 (
+            SYMBOL VARCHAR(16) NOT NULL,
+            NAME VARCHAR(16777216),
+            EXCHANGE VARCHAR(16777216),
+            primary key (SYMBOL)
+        );
+        '''
 )
 # DAG operator
 with DAG(
     DAG_ID,
     start_date=datetime(2024, 11, 7),
-    schedule_interval='0 0 * * *',
+    schedule_interval='*/5 * * * *',
     default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
     tags=['team6project2'],
     catchup=False,
 ) as dag:
     
-    snowflake_op_sql_str = SnowflakeOperator(
-        task_id='snowflake_op_sql_str',
-        sql=UPDATE_TABLE_SQL_STRING,
+    snowflake_op_sql_create_table = SnowflakeOperator(
+        task_id='snowflake_op_sql_create_table',
+        sql=CREATE_TABLE_SQL_STRING,
         warehouse=SNOWFLAKE_WAREHOUSE,
         database=SNOWFLAKE_DATABASE,
         schema=SNOWFLAKE_SCHEMA,
         role=SNOWFLAKE_ROLE,
     )
 
-    snowflake_op_sql_file = SnowflakeOperator(
-        task_id='snowflake_op_sql_file',
+    snowflake_op_sql_merge = SnowflakeOperator(
+        task_id='snowflake_op_sql_merge',
         sql=SQL_FILE,
         warehouse=SNOWFLAKE_WAREHOUSE,
         database=SNOWFLAKE_DATABASE,
@@ -49,7 +79,9 @@ with DAG(
     )
 
     (
-        snowflake_op_sql_str
-        >> snowflake_op_sql_file
+        snowflake_op_sql_create_table
+        >> [
+            snowflake_op_sql_merge,
+            ]
 
     )
--- project2/sql_op_str.sql
+++ project2/sql_op_str.sql
@@ -1,28 +1,54 @@
--- create a table based on the structure of source table but make it empty
-CREATE OR REPLACE TABLE FACT_STOCK_HISTORY_TEAM6_TEST AS
-    SELECT *
-    FROM US_STOCK_DAILY.DCCM.STOCK_HISTORY
-    WHERE 1=2;
-
--- check whether there are any data in the table
-SELECT * FROM FACT_STOCK_HISTORY_TEAM6_TEST;
-
--- insert data that are not in our table, in the case, it could be all
-INSERT INTO FACT_STOCK_HISTORY_TEAM6_TEST
-    SELECT *
-    FROM US_STOCK_DAILY.DCCM.STOCK_HISTORY
-    EXCEPT
-    SELECT *
-    FROM FACT_STOCK_HISTORY_TEAM6_TEST;
-
--- check where table are loaded successfully
-SELECT * FROM FACT_STOCK_HISTORY_TEAM6_TEST;
-
-
-
-
-
-
-
-
-
+MERGE INTO FACT_STOCK_HISTORY_TEAM6 AS target
+USING
+(
+    SELECT SYMBOL, DATE, OPEN, HIGH, LOW, CLOSE, VOLUME, ADJCLOSE
+    FROM
+    (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (PARTITION BY SYMBOL, DATE ORDER BY DATE) AS row_num
+        FROM US_STOCK_DAILY.DCCM.STOCK_HISTORY
+    ) AS deduped_source
+    WHERE row_num = 1
+) AS source
+ON target.SYMBOL = source.SYMBOL
+AND target.TRADEDATE = source.DATE
+WHEN MATCHED THEN
+    UPDATE SET
+        target.OPEN = source.OPEN,
+        target.HIGH = source.HIGH,
+        target.LOW = source.LOW,
+        target.CLOSE = source.CLOSE,
+        target.VOLUME = source.VOLUME,
+        target.ADJCLOSE = source.ADJCLOSE
+WHEN NOT MATCHED THEN
+    INSERT (SYMBOL, TRADEDATE, OPEN, HIGH, LOW, CLOSE, VOLUME, ADJCLOSE)
+    VALUES (source.SYMBOL, source.DATE, source.OPEN, source.HIGH, source.LOW, source.CLOSE, source.VOLUME, source.ADJCLOSE);
+SELECT * FROM DIM_COMPANY_PROFILE_TEAM6
+SELECT * FROM US_STOCK_DAILY.DCCM.COMPANY_PROFILE limit 100
+MERGE INTO DIM_COMPANY_PROFILE_TEAM6 AS target
+USING US_STOCK_DAILY.DCCM.COMPANY_PROFILE AS source
+    ON target.ID = source.ID
+    WHEN MATCHED THEN
+        UPDATE SET
+            target.SYMBOL = source.SYMBOL,
+            target.PRICE = source.PRICE,
+            target.BETA = source.BETA,
+            target.VOLAVG = source.VOLAVG,
+            target.INDUSTRY = source.INDUSTRY,
+            target.SECTOR = source.SECTOR,
+            target.WEBSITE = source.WEBSITE,
+            target.NAME = source.COMPANYNAME
+    WHEN NOT MATCHED THEN
+        INSERT (ID, SYMBOL, PRICE, BETA, VOLAVG, INDUSTRY, SECTOR,WEBSITE, NAME)
+        VALUES (source.ID, source.SYMBOL, source.PRICE, source.BETA, source.VOLAVG, source.INDUSTRY, source.SECTOR, source.WEBSITE, source.COMPANYNAME);
+MERGE INTO DIM_SYMBOLS_TEAM6 AS target
+USING US_STOCK_DAILY.DCCM.SYMBOLS AS source
+    ON target.SYMBOL = source.SYMBOL
+    WHEN MATCHED THEN
+        UPDATE SET
+            target.NAME = source.NAME,
+            target.EXCHANGE = source.EXCHANGE
+    WHEN NOT MATCHED THEN
+        INSERT (SYMBOL,NAME,EXCHANGE)
+        VALUES (source.SYMBOL, source.NAME, source.EXCHANGE);
