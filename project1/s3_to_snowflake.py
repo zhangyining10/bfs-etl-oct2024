7994cfefdb65b297a5aaad1366c647dd48cdb4c1
Yuxin-Ji <ji.yuxin@hotmail.com>
2024-11-07 18:42:48 -0600

    update project1

--- project1/s3_to_snowflake.py
+++ project1/s3_to_snowflake.py
@@ -19,7 +19,7 @@ with DAG(
     "project1_s3_to_snowflake",
     start_date = datetime(2024, 11, 6),
     end_date = datetime(2024, 11, 8),
-    schedule_interval='0 0 * * *', # send the data to snowflake every day at midnight
+    schedule_interval='*/5 * * * *', # send the data to snowflake every day at midnight
     default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
     tags=['team6project1'],
     catchup=True,
